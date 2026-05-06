d5ca9cad0fbcd6816f62a16c7d8d079a84dfdbce
Mayra <143430528+mayyzenacs@users.noreply.github.com>
2026-05-06 18:26:31 -0300

    switch database to sqlite

--- AmigoNcompra_api/Program.cs
+++ AmigoNcompra_api/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddCors(options => {
     options.AddDefaultPolicy(policy => 
-        policy.WithOrigins("http://localhost:5173")
+        policy.WithOrigins("")
               .AllowAnyMethod()
               .AllowAnyHeader());
 });
